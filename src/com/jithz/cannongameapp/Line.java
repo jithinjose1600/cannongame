a95ac09bef76725dc67a816ccdba5b7c49fd2c5c
Jithin Jose <'jithinjose1600@gmail.com'>
2015-03-01 23:04:31 -0500

    initial commit

---
+++ src/com/jithz/cannongameapp/Line.java
@@ -0,0 +1,9 @@
+package com.jithz.cannongameapp;
+
+import android.graphics.Point;
+
+public class Line
+{
+   public Point start = new Point(); // start Point--(0,0) by default
+   public Point end = new Point(); // end Point--(0,0) by default
+} // end class Line
